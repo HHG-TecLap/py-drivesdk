fce2c441731ea6ee73acf87954449f82230453dd
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-04-26 14:48:02 +0200

    Fixed anti-collider example (implemented hotifx

--- examples/NichtRammen/DONTCollideA4V0.py
+++ examples/NichtRammen/DONTCollideA4V0.py
@@ -1,8 +1,8 @@
+import sys, os
+sys.path.append(os.getcwd())
+
 import anki, asyncio, shiftRegister
 
-from anki.const import TrackPieceTypes
-
-from anki import track_pieces
 intersect1 = shiftRegister.shiftRegister()
 intersect2 = shiftRegister.shiftRegister()
 
@@ -13,7 +13,7 @@ async def Main():
     auto1 = await control.connect_one()
     auto2 = await control.connect_one()
     auto3 = await control.connect_one()
-    auto4 = await control.connect_one()
+    # auto4 = await control.connect_one()
 
     await control.scan()
 
@@ -22,8 +22,9 @@ async def Main():
         carOnMap.append(shiftRegister.shiftRegister())
 
 
-    def registering(vehicle, ID):
-        def regIntern (track):
+    def registering(vehicle : anki.Vehicle, ID):
+        def regIntern ():
+            track = vehicle.current_track_piece
             
             if vehicle.map_position in (2,8):
                 intersect1.addNew(ID)
@@ -43,12 +44,12 @@ async def Main():
     auto1.on_track_piece_change = registering(auto1,1)
     auto2.on_track_piece_change = registering(auto2,2)
     auto3.on_track_piece_change = registering(auto3,3)
-    auto4.on_track_piece_change = registering(auto4,4)
+    # auto4.on_track_piece_change = registering(auto4,4)
 
     await auto1.setSpeed(300)
     await auto2.setSpeed(300)
     await auto3.setSpeed(300)
-    await auto4.setSpeed(300)
+    # await auto4.setSpeed(300)
 
 
     async def carControl(vehicle, id, speed):
@@ -71,7 +72,7 @@ async def Main():
             await asyncio.sleep(0)
 
 
-    asyncio.create_task(carControl(auto4, 4, 400))
+    # asyncio.create_task(carControl(auto4, 4, 400))
     asyncio.create_task(carControl(auto3, 3, 300))
     asyncio.create_task(carControl(auto2, 2, 350))
     asyncio.create_task(carControl(auto1, 1, 300))
@@ -82,13 +83,10 @@ async def Main():
     try:
         while True:
             await asyncio.sleep(1)
-            #print(carOnMap[15].out(), carOnMap[16].out(), carOnMap[0].out(), carOnMap[1].out(), carOnMap[2].out())
-            
+            print(carOnMap[15].out(), carOnMap[16].out(), carOnMap[0].out(), carOnMap[1].out(), carOnMap[2].out())
             pass
     finally:
-        for v in control.vehicles:
-            await v.disconnect()
-            pass
+        await asyncio.gather([v.disconnect() for v in control.vehicles])
         pass
     pass
 
