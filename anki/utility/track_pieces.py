0c22e54cf09c0a5a84a45673ea2b0c1236c33983
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-02 16:16:40 +0200

    Added docstrings for TrackPiece class

--- anki/utility/track_pieces.py
+++ anki/utility/track_pieces.py
@@ -1,6 +1,15 @@
 from .const import TrackPieceTypes
 
 class TrackPiece():
+    """This class represents the different pieces of the track. It includes the type of the track piece and whether or not it turns clockwise.\n
+    You should not be creating these manually.
+
+    ## Parameters
+    + loc: The loc value send by the supercar. I'm not sure what it means either
+    + piece_val: The piece value sent by the supercar. The actual value might vary even when the type does not
+    + clockwise: An integer denoting whether or not this piece turns clockwise
+    """
+
     __slots__ = ["type","loc","clockwise"]
     def __init__(self, loc : int, piece_val : int, clockwise : int) -> None:
         object.__setattr__(self,"type",TrackPieceTypes.try_type(piece_val))
