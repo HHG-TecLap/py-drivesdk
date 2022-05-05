88251cff5769f0b615d84335b5a025fea3b4dc06
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-05 15:51:37 +0200

    Moved vehicle.py to control and changed imports

--- anki/__init__.py
+++ anki/__init__.py
@@ -1,4 +1,4 @@
-from .vehicle import Vehicle, Lights
+from .control.vehicle import Vehicle, Lights
 from .utility.track_pieces import TrackPiece, TrackPieceTypes
 from .control.controller import Controller
 from .utility.lanes import Lane3, Lane4
