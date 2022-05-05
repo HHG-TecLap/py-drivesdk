a8d0bed2942ec6066f910808d6f523cf37cc49c6
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2022-05-05 11:46:29 +0200

    repaired stuff

--- examples/UiCode/Ui copy.py
+++ examples/UiCode/Ui copy.py
@@ -10,6 +10,7 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 control = anki.Controller()
 map = []
 
+
 STOP = False #Setting this to TRUE will make your programm stop existing!!!
 
 async def ankiMain(): 
@@ -102,14 +103,15 @@ def gen_vismap():
 
 
 
-
+logSurface = logHistory = ...
 
 def Uimain():
+    global logSurface, logHistory, font
     global STOP
     pygame.init()
-    Ui = pygame.display.set_mode((1000,600),pygame.RESIZABLE)
+    Ui = pygame.display.set_mode((1000,600),pygame.SCALED)
     Logo = pygame.image.load("Logo.png")
-    pygame.display.set_caption("Anki Ui Acsess")
+    pygame.display.set_caption("Anki Ui Access")
     pygame.display.set_icon(Logo)
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("Arial",20)
@@ -123,13 +125,18 @@ def Uimain():
     mapxSize = len(vismap)*100
     mapYSize = len(vismap[0])*100
     mapsurface = pygame.Surface((mapxSize, mapYSize)) 
-    mapsurface.fill((193, 60, 60))
+    mapsurface.fill((98, 60, 73))
     carPosSurface = pygame.Surface((mapxSize, mapYSize), pygame.SRCALPHA)
     carPosMap = [None]*(mapxSize//100) #nen array mit nem array und nem array
     for i in range(len(carPosMap)):
         carPosMap[i] = [None]*(mapYSize//100)
         for o in range(len(carPosMap[i])):
             carPosMap[i][o] = []
+    logSurface = pygame.Surface((100,150))
+    logHistory = [None]*5
+    for i in range(len(logHistory)):
+        logHistory[i] = font.render("", True, (0,0,0))
+
     for x in range(len(vismap)):
         for y in range(len(vismap[x])):
             if vismap[x][y] is not None  :
@@ -151,19 +158,20 @@ def Uimain():
     for i in range(len(autos)):
         autoData.append(pygame.Surface((100,100)))
     
+    
     while run and not STOP:
         Ui.fill((81, 255, 174))
         Ui.blit(pygame.transform.flip(mapsurface, True, False),(0,0))
         for x in range(len(autos)):
             work_surface = autoData[x]
-            work_surface.fill((81, 255, 174))
-            auto_name = font.render(CarDict[autos[x].id], True, (0,0,0))
-            auto_id = font.render(str(autos[x].id), True, (0,0,0))
-            auto_pos = font.render(str(autos[x].map_position), True, (0,0,0))
+            work_surface.fill((135, 237, 208))
+            auto_name = font.render(f"Name: {CarDict[autos[x].id]}", True, (0,0,0))
+            auto_id = font.render(f"ID: {autos[x].id}", True, (0,0,0))
+            auto_pos = font.render(f"Position: {autos[x].map_position}", True, (0,0,0))
             
-            auto_piece = font.render(anki.TrackPieceTypes.as_str(autos[x].current_track_piece.type), True, (0,0,0))
+            auto_piece = font.render(f"Streckenteil Typ:{anki.TrackPieceTypes.as_str(autos[x].current_track_piece.type)}", True, (0,0,0))
             
-            auto_lane = font.render(str(autos[x].current_lane4), True, (0,0,0))
+            auto_lane = font.render(f"Spur: {autos[x].current_lane4}", True, (0,0,0))
             surf_height = 40 + sum([surf.get_height() for surf in (auto_name, auto_pos, auto_piece, auto_lane)])
             #surf_width = max([surf.get_width() for surf in (auto_name, auto_pos, auto_piece)])
             if surf_height != work_surface.get_height():
@@ -176,10 +184,10 @@ def Uimain():
                 (auto_piece,(0, auto_name.get_height() + auto_pos.get_height())),
                 (auto_lane,(0, auto_name.get_height() + auto_pos.get_height() + auto_piece.get_height()))
             ))
-            
+            pygame.draw.line(work_surface, (98, 60, 73), (0, work_surface.get_height()),(work_surface.get_width(), work_surface.get_height()), 20)
             Ui.blit(work_surface, (mapxSize, x*work_surface.get_height()))
             
-        carPosSurface.fill((255,255,255, 0) )
+        carPosSurface.fill((255,255,255, 0))
         for x in range(len(vismap)):
             for y in range(len(vismap[x])):
                 if vismap[x][y] is not None:
@@ -194,17 +202,45 @@ def Uimain():
                                     carPosMap[x][y].append(a.id)
         for x in range(len(carPosMap)):
             for y in range(len(carPosMap[x])):
-                for z in carPosMap[x][y]:
-                    carPosSurface.blit(font.render(str(z), True, (100,100,100)), (x*100+z*20, y*100))
+                for vehicle_num in range(len(carPosMap[x][y])):
+                    carPosSurface.blit(
+                        pygame.transform.flip(
+                            font.render(
+                                str(carPosMap[x][y][vehicle_num]), 
+                            True, 
+                            (245,255,84)
+                            )
+                            ,True, False),
+                            (x*100+80-vehicle_num*20, y*100+70)
+                        )
                 carPosMap[x][y].clear()
         Ui.blit(pygame.transform.flip(carPosSurface,True, False),(0,0))
+        Ui.blit(logSurface,(0,mapsurface.get_height()))
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(10)
     STOP = True #Stopping programm 
+
+def enterLog(logEntry, color = (0,0,0)):
+    global logSurface, logHistory
+    width = 0
+    for i in reversed(range(len(logHistory)-1)):
+        logHistory[i+1] = logHistory[i]
+        if width < logHistory[i].get_width():
+            width = logHistory[i].get_width()
+    logHistory[0] = font.render(str(logEntry), True, color)
+    if width < logHistory[0].get_width():
+        width = logHistory[0].get_width()
+
+    logSurface = pygame.Surface((width, 150))
+    logSurface.fill((117,164,120))
+    for i in range(len(logHistory)):
+        logSurface.blit(logHistory[i], (0, i*30))
+        print(i, logHistory[i])
+
 
 
 
@@ -218,6 +254,10 @@ async def setup():
     # await asyncio.sleep(40)
     frontend = threading.Thread(target = Uimain)
     frontend.start()
+    await asyncio.sleep(1)
+    enterLog("Setup System!", (0,255,255))
+    for v in autos:
+        enterLog(f"Vehicle {CarDict[v.id]} ready!")
 
     try:
         while not STOP:
@@ -225,6 +265,7 @@ async def setup():
             pass
     except KeyboardInterrupt: pass
     finally:
+        await asyncio.gather(*[v.stop() for v in autos])
         await control.disconnectAll()
     pass
 
