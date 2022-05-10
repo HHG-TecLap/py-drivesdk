bbcd7c097aed8d6802a89534532f06fb466c7ee9
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2022-05-10 11:08:15 +0200

    Vehicle.stop() was canged

--- anki/control/vehicle.py
+++ anki/control/vehicle.py
@@ -209,7 +209,7 @@ class Vehicle:
 
     async def stop(self):
         """Stops the Supercar"""
-        await self.setSpeed(0) # stop = 0 speed
+        await self.setSpeed(0, 600) # stop = 0 speed
         pass
 
     async def changeLane(self, lane : _Lane, horizontalSpeed : int = 300, horizontalAcceleration : int = 300, *, _hopIntent : int = 0x0, _tag : int = 0x0):
