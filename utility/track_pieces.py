703079b3a793fb4724ea4a5e2e0571ce24937196
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-29 13:25:35 +0200

    Attempting to fix merge conflict in track_pieces

--- utility/track_pieces.py
+++ utility/track_pieces.py
@@ -4,6 +4,13 @@ from dataclasses import dataclass, field
 
 @dataclass(frozen=True, slots=True)
 class TrackPiece():
+    """This class represents the different pieces of the track. It includes the type of the track piece and whether or not it turns clockwise.\n
+    You should not be creating these manually.
+    ## Parameters
+    + loc: The loc value send by the supercar. I'm not sure what it means either
+    + piece_val: The piece value sent by the supercar. The actual value might vary even when the type does not
+    + clockwise: An integer denoting whether or not this piece turns clockwise
+    """
     loc : int
     type : TrackPieceType
     clockwise : bool = field(compare=False)
