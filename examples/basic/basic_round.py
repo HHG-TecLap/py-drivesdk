37719a18a3bb43415abcb635263a463d941487b2
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-09-22 11:58:39 +0200

    Reworked basic_round to be more pythonic

--- examples/basic/basic_round.py
+++ examples/basic/basic_round.py
@@ -1,6 +1,7 @@
 """
-This example demonstrates the basic setup for single-vehicle programs.
-It connects to a non-charging vehicle, drives along the track until it reaches the finish, and then disconnects cleanly.
+This example expands upon basic_single. 
+Here, the vehicle drives 1 round of the track, before stopping.
+The concept of vehicle alignment and the `Vehicle.wait_for_track_change` method are demonstrated.
 """
 
 import anki, asyncio
@@ -11,19 +12,18 @@ async def main():
     controller = anki.Controller() # This object handles all the connections to the vehicles
 
     vehicle = await controller.connectOne() # This object represents the vehicle and is used to control it.
-
-    finished = [False]
+    
+    await vehicle.align() # Align to the START piece. This will allow us to use Vehicle.map_position later
     await vehicle.setSpeed(300) # Accelerate the vehicle to 300mm/s
-    def watcher(): # Function to check if the vehicle is on the Finish-piece
-        track = vehicle._current_track_piece # You shouldn't use this property, because it's an internal property
-        if track.type == TrackPieceTypes.FINISH:
-            finished[0] = True
-        else:
-            pass
-    vehicle.on_track_piece_change = watcher # Assign the watcher function to the method that gets executed when the track-piece changes
-    while not finished[0]:
-        await asyncio.sleep(1)
+
+    await vehicle.wait_for_track_change() # Wait for the vehicle to reach a new position.
+    # We do this once before the loop so that we leave the START piece before checking if we are on the piece.
+    while vehicle.map_position == 0: # This while-loop runs until the vehicle is at position 0, which is always the START piece.
+        print("The vehicle is currently on position",vehicle.map_position) # Print out the current position of the vehicle
+        await vehicle.wait_for_track_change() # Wait for the vehicle to reach a new position. 
+        # We won't actually get a track piece out of this since the map is not scanned in.
         pass
+    
     await vehicle.stop() # Stop the vehicle
     await vehicle.disconnect() # Always disconnect the vehicle. Otherwise it may bug into a state where it won't reconnect.
     pass
