d26243f73f76cc22781a99edd4f6c70ef81b51e6
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-09-29 16:07:17 +0200

    Removed broken import

--- anki/__init__.py
+++ anki/__init__.py
@@ -1,5 +1,5 @@
 from .control.vehicle import Vehicle, Lights
-from .utility.track_pieces import TrackPiece, TrackPieceTypes
+from .utility.track_pieces import TrackPiece
 from .control.controller import Controller
 from .utility.lanes import Lane3, Lane4
 from . import errors
