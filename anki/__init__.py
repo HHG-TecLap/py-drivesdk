3866e9dbf6ceda865190f79fb3d6fd9c3a6f71ba
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-09-29 16:20:54 +0200

    Added base import for TrackPieceType

--- anki/__init__.py
+++ anki/__init__.py
@@ -1,5 +1,5 @@
 from .control.vehicle import Vehicle, Lights
-from .utility.track_pieces import TrackPiece
+from .utility.track_pieces import TrackPiece, TrackPieceType
 from .control.controller import Controller
 from .utility.lanes import Lane3, Lane4
 from . import errors
