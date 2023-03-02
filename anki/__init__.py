6b373349b9d4ed1e07ae8b337b7e72cd0c2735a3
PascalRombach <pasc.rombach@heinrichheinedo.de>
2023-03-02 10:20:46 +0100

    Repeat

--- anki/__init__.py
+++ anki/__init__.py
@@ -1,8 +1,8 @@
+import os
+print(os.listdir(os.path.dirname(__file__)))
+
 from .control.vehicle import Vehicle, Lights
 from .misc.track_pieces import TrackPiece, TrackPieceType
 from .control.controller import Controller
 from .misc.lanes import Lane3, Lane4, BaseLane
 from . import errors
-
-import os
-print(os.listdir(os.path.dirname(__file__)))
