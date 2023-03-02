654e59f3dc1d0ad669e59b44e0284e8b01fb9fff
PascalRombach <pasc.rombach@heinrichheinedo.de>
2023-03-02 10:15:26 +0100

    Debugging failing docs

--- anki/__init__.py
+++ anki/__init__.py
@@ -3,3 +3,6 @@ from .misc.track_pieces import TrackPiece, TrackPieceType
 from .control.controller import Controller
 from .misc.lanes import Lane3, Lane4, BaseLane
 from . import errors
+
+import os
+print(os.listdir(os.path.dirname(os.getcwd())))
