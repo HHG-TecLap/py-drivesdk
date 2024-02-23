217193dc09a870abb4d207a57f1a99182884a15d
PascalRombach <pasc.rombach@heinrichheinedo.de>
2024-02-23 12:05:25 +0100

    Added documentation to utils.recover_delocalization

--- anki/utils/handlers.py
+++ anki/utils/handlers.py
@@ -25,7 +25,42 @@ async def _handle_delocalization_task(
     await v.set_speed(recovery_speed)
     logging.debug("Recovery successful. Vehicle restarted")
 
-def recover_delocalization(v: Vehicle, recovery_speed: int|None=None):
+def recover_delocalization(vehicle: Vehicle, recovery_speed: int|None=None):
+    """
+    Generates and registers a callback that 
+    automatically handles a delocalized vehicle.
+
+    It does this by creating a new task when a vehicle is delocalized.
+    This task repeatedly checks if the vehicle has **not** send a
+    delocalization packages in the last 2 seconds and attempts to restart
+    the vehicle in that case. Otherwise it continues to wait.
+    
+    :param vehicle: :class:`Vehicle`
+        The :class:`Vehicle` to register the callback to.
+    :param recovery_speed: :class:`Optional[int]`
+        The speed with which to recover from delocalization.
+        If set to :const:`None`, the task will use the 
+        last recorded speed.
+
+        .. note::
+            Leaving this value as :const:`None` may gradually
+            decrease vehicle speed over long periods of time,
+            if speed is not set regularly.
+    
+    Returns
+    -------
+    :class:`Callable[[], None]`
+
+    The registered callback. Using this as an argument to
+    :func:`Vehicle.remove_delocalized_watcher` will remove
+    the automatic delocalization recovery.
+    Executing this function will start it.
+
+    .. warn::
+        It is highly adviced not to manually start recovery
+        unless you are absolutely certain the vehicle has 
+        delocalized.
+    """
     task: asyncio.Task|None = None
     last_callback = Reference(0.0)
     @vehicle.delocalized
@@ -33,6 +68,6 @@ def recover_delocalization(v: Vehicle, recovery_speed: int|None=None):
         nonlocal task
         last_callback.value = monotonic()
         if task is None or task.done():
-            task = asyncio.create_task(_handle_delocalization_task(v, last_callback, recovery_speed))
+            task = asyncio.create_task(_handle_delocalization_task(vehicle, last_callback, recovery_speed))
         logging.warning("Vehicle has delocalized. Attempting automatic reovery")
     return callback
